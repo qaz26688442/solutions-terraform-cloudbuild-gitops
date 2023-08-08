f7bcae8b12fcf8d99b4420f125d926284a22fb48
林菘彬 <qaz26688442@hotmail.com>
2023-08-08 23:18:17 +0800

    Update project IDs and buckets

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "eternal-centaur-393301-tfstate"
     prefix = "env/prod"
   }
 }
--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="eternal-centaur-393301"
